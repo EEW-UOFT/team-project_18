1881a0a44292611f41e9bda198ebaa1dffeab5ef
Ryan-Xue2 <ryan.xue2@gmail.com>
2025-11-10 09:51:11 -0500

    created card class

---
+++ src/main/java/Card.java
@@ -0,0 +1,17 @@
+public class Card {
+    private final String suit;
+    private final int value;
+
+    public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return this.suit;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
