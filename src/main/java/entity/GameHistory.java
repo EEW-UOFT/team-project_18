7d10aa56b37b490a40398d456ed4217488893f59
hleix <helenleix@gmail.com>
2025-11-21 13:02:16 -0500

    GameHistory class implemented

---
+++ src/main/java/entity/GameHistory.java
@@ -0,0 +1,21 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameHistory {
+
+    private List<HistoryEntry> history;
+
+    public GameHistory(){
+        this.history = new ArrayList<>();
+    }
+
+    public void addEntry(HistoryEntry entry){
+        this.history.add(entry);
+    }
+
+    public List<HistoryEntry> getHistory() {
+        return history;
+    }
+}
