b65be7395cad3c58d40f1d84be1d0b121536404d
sunnyshanni <sunnyshanni.cai@mail.utoronto.ca>
2025-11-22 21:54:05 -0500

    updated a ViewManagerModel and refractor the change

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -5,25 +5,31 @@ import entity.User;
 import interface_adapter.StartNewGame.StartNewGameController;
 import interface_adapter.StartNewGame.StartNewGamePresenter;
 import interface_adapter.StartNewGame.StartNewGameViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.StartNewGame.StartNewGameInputBoundary;
 import use_case.StartNewGame.StartNewGameInteractor;
+import use_case.StartNewGame.StartNewGameOutputBoundary;
 import view.HomePageView;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class AppBuilder {
 
     private StartNewGameController startNewGameController;
     private StartNewGameViewModel startNewGameViewModel;
+    private ViewManagerModel viewManagerModel;
 
     public AppBuilder() {
+        this.viewManagerModel = new ViewManagerModel();
         addStartNewGameUseCase();
     }
 
     public void addStartNewGameUseCase() {
-        StartNewGameViewModel viewModel = new StartNewGameViewModel(null);
-        StartNewGamePresenter presenter = new StartNewGamePresenter(viewModel);
-        StartNewGameInteractor interactor = new StartNewGameInteractor(presenter);
+        StartNewGameViewModel viewModel = new StartNewGameViewModel();
+        StartNewGameOutputBoundary presenter = new StartNewGamePresenter(viewModel, viewManagerModel);
+        StartNewGameInputBoundary interactor = new StartNewGameInteractor(presenter);
         ArrayList<HistoryEntry> history = new ArrayList<>();
         StartNewGameController controller = new StartNewGameController(interactor, new User(history));
         this.startNewGameController = controller;
@@ -32,9 +38,23 @@ public class AppBuilder {
 
     JFrame build() {
         JFrame frame = new JFrame("BlackJack");
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel mainPanel = new JPanel(cardLayout);
+
+
         HomePageView homePage = new HomePageView(startNewGameViewModel, startNewGameController);
 
-        frame.add(homePage);
+        mainPanel.add(homePage,"HomePage");
+
+        viewManagerModel.addPropertyChangeListener(evt ->{
+            if("view".equals(evt.getPropertyName())){
+                String activeView = (String) evt.getNewValue();
+                cardLayout.show(mainPanel, activeView);
+            }
+        });
+
+        frame.add(mainPanel);
         frame.setSize(250, 150);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         System.out.println("hi");
--- src/main/java/interface_adapter/StartNewGame/StartNewGamePresenter.java
+++ src/main/java/interface_adapter/StartNewGame/StartNewGamePresenter.java
@@ -1,19 +1,23 @@
 package interface_adapter.StartNewGame;
 
+import interface_adapter.ViewManagerModel;
 import use_case.StartNewGame.StartNewGameOutputBoundary;
 import use_case.StartNewGame.StartNewGameOutputData;
 
 public class StartNewGamePresenter implements StartNewGameOutputBoundary {
 
     private final StartNewGameViewModel startNewGameViewModel;
+    private final ViewManagerModel viewManagerModel;
 
-    public StartNewGamePresenter(StartNewGameViewModel startNewGameViewModel) {
+    public StartNewGamePresenter(StartNewGameViewModel startNewGameViewModel, ViewManagerModel viewManagerModel) {
         this.startNewGameViewModel = startNewGameViewModel;
+        this.viewManagerModel = viewManagerModel;
     }
 
     @Override
     public void prepareSuccessView(StartNewGameOutputData response){
         startNewGameViewModel.setCurrentGame(response.getCurrentGame());
+        viewManagerModel.setActiveView("game");
     }
 
     @Override
