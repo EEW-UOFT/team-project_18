154ec9052e3407141583dd75aa247866082485d2
Ryan-Xue2 <ryan.xue2@gmail.com>
2025-11-28 22:55:19 -0500

    didn't add this to last commit on accident

--- src/main/java/interfaceadapter/gamerule/GameRulePresenter.java
+++ src/main/java/interfaceadapter/gamerule/GameRulePresenter.java
@@ -1,4 +1,13 @@
 package interfaceadapter.gamerule;
 
-public class GameRulePresenter {
+import use.Case.gamerule.GameRuleOutputBoundary;
+
+public class GameRulePresenter implements GameRuleOutputBoundary {
+    private final GameRuleViewModel gameRuleViewModel;
+    public GameRulePresenter(GameRuleViewModel gameRuleViewModel) {
+        this.gameRuleViewModel = gameRuleViewModel;
+    }
+    public void presentGameRules(String rules) {
+        this.gameRuleViewModel.setGameRules(rules);
+    }
 }
--- src/main/java/interfaceadapter/gamerule/GameRuleViewModel.java
+++ src/main/java/interfaceadapter/gamerule/GameRuleViewModel.java
@@ -1,4 +1,13 @@
 package interfaceadapter.gamerule;
 
+import use.Case.stand.StandOutputData;
+
 public class GameRuleViewModel {
+    private String gameRules;
+    public String getGameRules() {
+        return gameRules;
+    }
+    public void setGameRules(String gameRules) {
+        this.gameRules = gameRules;
+    }
 }
